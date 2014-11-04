cd10473c114d8a5ef92fb4bcb829e34c42085e45
Elly <9212227@gmail.com>
2014-11-04 21:01:43 +0000

    Used schema builder in migrations to define data structure of scrabble games and players

--- laravel/app/database/migrations/2014_11_04_204250_create_players.php
+++ laravel/app/database/migrations/2014_11_04_204250_create_players.php
@@ -7,22 +7,30 @@ class CreatePlayers extends Migration {
 
 	/**
 	 * Run the migrations.
+	 * Create the players, giving them a name and contact number
 	 *
 	 * @return void
 	 */
 	public function up()
 	{
-		//
+		Schema::create('players', function($newtable)
+		{
+			$newtable->increments('id');
+			$newtable->string('name');
+			$newtable->string('contact_number');
+			$newtable->timestamps();
+		});
 	}
 
 	/**
 	 * Reverse the migrations.
-	 *
+	 * Drop the table
+	 * 
 	 * @return void
 	 */
 	public function down()
 	{
-		//
+		Schema::drop('players');
 	}
 
 }
--- laravel/app/database/migrations/2014_11_04_204256_create_games.php
+++ laravel/app/database/migrations/2014_11_04_204256_create_games.php
@@ -7,22 +7,34 @@ class CreateGames extends Migration {
 
 	/**
 	 * Run the migrations.
+	 * Creates the games migration, with each of the
+	 * players in the game linked to the players table
 	 *
 	 * @return void
 	 */
 	public function up()
 	{
-		//
+		Schema::create('games', function($table)
+		{
+			$table->increments('id');
+			$table->string('location');
+			$table->integer('player1_id')->unsigned();
+			$table->foreign('player1_id')->references('id')->on('players');
+			$table->integer('player2_id')->unsigned();
+			$table->foreign('player2_id')->references('id')->on('players');
+			$table->timestamps();
+		});
 	}
 
 	/**
 	 * Reverse the migrations.
-	 *
+	 * Drop the table
+	 * 
 	 * @return void
 	 */
 	public function down()
 	{
-		//
+		Schema::drop('games');
 	}
 
 }
